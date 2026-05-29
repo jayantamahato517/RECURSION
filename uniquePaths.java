bd303fbec98c7171638c9c1b0edb780a0173919a
Jayanta Mahato <mahatojayanta69@gmail.com>
2026-05-29 12:24:33 +0530

    Updated project

--- uniquePaths.java
+++ uniquePaths.java
@@ -1,13 +1,21 @@
 class uniquePaths{
-    public static int UniquePaths(int m , int n){
-        if(m == 1 || n == 1){
+    public static int countPaths(int i, int j, int m , int n){
+        //base case
+        //if destination is reached
+        if(i == m-1 && j == n-1){
             return 1;
         }
-        return UniquePaths(m-1, n) + UniquePaths(m, n-1);
+        //if out of boundary
+        if(i >= m || j >= n){
+            return 0;
+        }
+        //move down + move right
+        return countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n);
     }
     public static void main(String[] args) {
         int m = 3;
         int n = 3;
-        System.out.println(UniquePaths(m, n));
+        int result = countPaths(0,0,m, n);
+        System.out.println("Uniue Paths : " + result);
     }
 }
